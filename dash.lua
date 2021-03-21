8d680bf8a5263121df4d1992d2991de66cd709d9
shagu <ericmauser@googlemail.com>
2021-03-21 01:28:03 +0100

    dash: align text to maximum string lengths

--- dash.lua
+++ dash.lua
@@ -7,13 +7,32 @@ local output = {
   }
 }
 
+local width = {}
+local function scanwidth(tbl)
+  for str, data in pairs(tbl) do
+    width[tbl] = math.max((width[tbl] or 0), string.len(str))
+    if type(data) == "table" and not width[data] then scanwidth(data) end
+  end
+end
+
+local function getspace(context, str)
+  local num = width[context] - string.len(str)
+  local space = ""
+  for i=0,num do space = space .. " " end
+  return space, num
+end
+
 local function draw()
+  scanwidth(output)
+
+  -- draw content to screen
   io.write("\027[H\027[2J")
-  for category, data in pairs(output) do
+  for category, elements in pairs(output) do
     print("\027[1m\027[34m:: \027[0m\027[1m" .. category .. "\027[0m")
-    for title, data in pairs(data) do
+    for title, data in pairs(elements) do
       local value = data.value and data.value .. (data.append or "") or "N/A"
-      print("  " .. title .. ": " .. value)
+      local spacing, num = getspace(elements, title)
+      print("  " .. title .. ": " .. spacing .. value)
     end
   end
 end
