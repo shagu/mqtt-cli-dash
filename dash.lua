78f343c4b876e64b4fd94fbea9774f23090ed957
shagu <ericmauser@googlemail.com>
2021-03-21 01:55:41 +0100

    dash: use ordered table structure

--- dash.lua
+++ dash.lua
@@ -1,45 +1,65 @@
 #!/bin/env lua
 local output = {
-  ["Heizung"] = {
-    ["Schlafzimmer"] = { topic = "hm/status/schlafzimmer-heizung:1/ACTUAL_TEMPERATURE", json = { "val" }, append = "°C" },
-    ["Bad"]          = { topic = "hm/status/bad-heizung:1/ACTUAL_TEMPERATURE",          json = { "val" }, append = "°C" },
-    ["Gästezimmer"]  = { topic = "hm/status/gästezimmer-heizung:1/ACTUAL_TEMPERATURE",  json = { "val" }, append = "°C" },
-  }
+  { title = "Heizung", elements = {
+    { title = "Bad",           topic = "hm/status/bad-heizung:1/ACTUAL_TEMPERATURE",            json = { "val" }, append = "°C" },
+    { title = "Schlafzimmer",  topic = "hm/status/schlafzimmer-heizung:1/ACTUAL_TEMPERATURE",   json = { "val" }, append = "°C" },
+    { title = "Gästezimmer",   topic = "hm/status/gästezimmer-heizung:1/ACTUAL_TEMPERATURE",    json = { "val" }, append = "°C" },
+    { title = "Arbeitszimmer", topic = "hm/status/arbeitszimmer-heizung:1/ACTUAL_TEMPERATURE",  json = { "val" }, append = "°C" },
+  }}
 }
+
+local function utflen(str)
+  local str = tostring(str)
+  local length, utf = #str, 0
+  for i=1,#str do
+    local char = string.sub(str, i, i)
+    if string.byte(char) >= 194 then
+      utf = utf + 1
+    end
+  end
+
+  return length - utf
+end
 
 local width = {}
 local function scanwidth(tbl)
-  for str, data in pairs(tbl) do
-    width[tbl] = math.max((width[tbl] or 0), string.len(str))
-    if type(data) == "table" and not width[data] then scanwidth(data) end
+  for id, data in pairs(tbl) do
+    if type(data) == "table" and data.title then
+      width[tbl] = math.max((width[tbl] or 0), utflen(data.title))
+      for id, dat in pairs(data) do
+        if type(dat) == "table" and not width[dat] then scanwidth(dat) end
+      end
+    end
   end
 end
 
 local function getspace(context, str)
-  local num = width[context] - string.len(str)
+  local num = width[context] - utflen(str)
   local space = ""
   for i=0,num do space = space .. " " end
   return space, num
 end
 
 local function draw()
+  -- scan all title widths
   scanwidth(output)
 
   -- draw content to screen
   io.write("\027[H\027[2J")
-  for category, elements in pairs(output) do
-    print("\027[1m\027[34m:: \027[0m\027[1m" .. category .. "\027[0m")
-    for title, data in pairs(elements) do
+  for id, section in pairs(output) do
+    print("\027[1m\027[34m:: \027[0m\027[1m" .. section.title .. "\027[0m")
+
+    for id, data in pairs(section.elements) do
       local value = data.value and data.value .. (data.append or "") or "N/A"
-      local spacing, num = getspace(elements, title)
-      print("  " .. title .. ": " .. spacing .. value)
+      local spacing, num = getspace(section.elements, data.title)
+      print("  " .. data.title .. ": " .. spacing .. value)
     end
   end
 end
 
 local listen = {}
-for category, data in pairs(output) do
-  for title, data in pairs(data) do
+for id, data in pairs(output) do
+  for id, data in pairs(data.elements) do
     listen[data.topic] = listen[data.topic] or {}
     table.insert(listen[data.topic], data)
   end
